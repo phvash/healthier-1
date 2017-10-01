eb233047a6951b322667d70f8d009e675282fc57
Phvash <davash001@gmail.com>
2017-10-01 23:57:24 +0100

    update db config

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -1,33 +1,34 @@
 import os
 import sys
 
-from .base import *
+from .local import *
 
 
+DATABASES['default'] = env.db('CLEARDB_DATABASE_URL')
 
-try:
+# try:
 
-    # Check to make sure DATABASES is set in settings.py file.
-    # If not default to {}
+#     # Check to make sure DATABASES is set in settings.py file.
+#     # If not default to {}
 
-    if 'DATABASES' not in locals():
-        DATABASES = {}
+#     if 'DATABASES' not in locals():
+#         DATABASES = {}
 
-    if 'DATABASE_URL' in os.environ:
+#     if 'DATABASE_URL' in os.environ:
 
-        # Ensure default database exists.
-        DATABASES['default'] = DATABASES.get('default', {})
+#         # Ensure default database exists.
+#         DATABASES['default'] = DATABASES.get('default', {})
 
-        # Update with environment configuration.
-        DATABASES['default'].update({
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USERNAME'),
-            'PASSWORD': os.environ.get('DB_PWD'),
-            'HOST': os.environ.get('DB_HOST'),
-        })
+#         # Update with environment configuration.
+#         DATABASES['default'].update({
+#             'NAME': os.environ.get('DB_NAME'),
+#             'USER': os.environ.get('DB_USERNAME'),
+#             'PASSWORD': os.environ.get('DB_PWD'),
+#             'HOST': os.environ.get('DB_HOST'),
+#         })
 
 
-        if url.scheme == 'mysql':
-            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-except Exception:
-    print('Unexpected error:', sys.exc_info())
+#         if url.scheme == 'mysql':
+#             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+# except Exception:
+#     print('Unexpected error:', sys.exc_info())
