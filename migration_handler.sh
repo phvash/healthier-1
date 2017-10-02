a6b18ec48261d2250f561ca4a5a71c33593a3847
Phvash <davash001@gmail.com>
2017-10-02 01:19:56 +0100

    update migration handler

--- migration_handler.sh
+++ migration_handler.sh
@@ -1,20 +1,28 @@
-#!/usr/bin/env bash
+# #!/usr/bin/env bash
 
-echo "Starting ..."
+# echo "Starting ..."
 
-echo ">> Deleting old migrations"
-find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find . -path "*/migrations/*.pyc"  -delete
+# echo ">> Deleting old migrations"
+# find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+# find . -path "*/migrations/*.pyc"  -delete
 
 
-# Optional
-echo ">> Deleting database"
-find . -name "db.sqlite3" -delete
+# # Optional
+# echo ">> Deleting database"
+# find . -name "db.sqlite3" -delete
 
-echo ">> Running manage.py makemigrations"
-python manage.py makemigrations
+# echo ">> Running manage.py makemigrations"
+# python manage.py makemigrations
 
-echo ">> Running manage.py migrate"
-python manage.py migrate
+# echo ">> Running manage.py migrate"
+# python manage.py migrate
 
-echo ">> Done"
+# echo ">> Done"
+
+python manage.py makemigrations user
+python manage.py makemigrations consumers
+python manage.py makemigrations providers
+python manage.py makemigrations service
+python manage.py makemigrations auth
+
+python manage.py migrate 
