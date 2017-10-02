b27ada7156413b5e8fffd73ce205ad382d1bfdc2
Phvash <davash001@gmail.com>
2017-10-02 01:32:45 +0100

    update staging settings

--- config/settings/staging.py
+++ config/settings/staging.py
@@ -6,29 +6,41 @@ from .local import *
 
 DATABASES['default'] = env.db('CLEARDB_DATABASE_URL')
 
-# try:
-
-#     # Check to make sure DATABASES is set in settings.py file.
-#     # If not default to {}
-
-#     if 'DATABASES' not in locals():
-#         DATABASES = {}
-
-#     if 'DATABASE_URL' in os.environ:
-
-#         # Ensure default database exists.
-#         DATABASES['default'] = DATABASES.get('default', {})
-
-#         # Update with environment configuration.
-#         DATABASES['default'].update({
-#             'NAME': os.environ.get('DB_NAME'),
-#             'USER': os.environ.get('DB_USERNAME'),
-#             'PASSWORD': os.environ.get('DB_PWD'),
-#             'HOST': os.environ.get('DB_HOST'),
-#         })
+# This ensures that Django will be able to detect a secure connection
+# properly on Heroku.
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# raven sentry client
+# See https://docs.sentry.io/clients/python/integrations/django/
+INSTALLED_APPS += ['raven.contrib.django.raven_compat', ]
 
 
-#         if url.scheme == 'mysql':
-#             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
-# except Exception:
-#     print('Unexpected error:', sys.exc_info())
+# Use Whitenoise to serve static files
+# See: https://whitenoise.readthedocs.io/
+WHITENOISE_MIDDLEWARE = ['whitenoise.middleware.WhiteNoiseMiddleware', ]
+MIDDLEWARE = WHITENOISE_MIDDLEWARE + MIDDLEWARE
+RAVEN_MIDDLEWARE = ['raven.contrib.django.raven_compat.middleware.SentryResponseErrorIdMiddleware']
+MIDDLEWARE = RAVEN_MIDDLEWARE + MIDDLEWARE
+
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['example.com', ])
+# END SITE CONFIGURATION
+
+INSTALLED_APPS += ['gunicorn', ]
+
+# Static Assets
+# ------------------------
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# COMPRESSOR
+# ------------------------------------------------------------------------------
+COMPRESS_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+COMPRESS_URL = STATIC_URL
+COMPRESS_ENABLED = env.bool('COMPRESS_ENABLED', default=True)
+
+# TEMPLATE CONFIGURATION
+# ------------------------------------------------------------------------------
+# See:
+# https://docs.djangoproject.com/en/dev/ref/templates/api/#django.template.loaders.cached.Loader
+TEMPLATES[0]['OPTIONS']['loaders'] = [
+    ('django.template.loaders.cached.Loader', [
+        'django.template.loaders.filesystem.Loader', 'django.template.loaders.app_directories.Loader', ]),
+]
