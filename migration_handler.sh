63594c51cd0cda047d59d9f3965d0be116706fa1
Phvash <davash001@gmail.com>
2017-10-05 20:34:02 +0100

    fix django message

--- migration_handler.sh
+++ migration_handler.sh
@@ -23,6 +23,7 @@ python manage.py makemigrations user
 python manage.py makemigrations consumers
 python manage.py makemigrations providers
 python manage.py makemigrations service
+python manage.py makemigrations messenger
 python manage.py makemigrations auth
 
 python manage.py migrate 
